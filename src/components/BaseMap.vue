bff059c2fd0a7ce34b70cb32f73049d680bbc085
larsmarginet <lars.marginet@student.howest.be>
2023-08-07 19:57:17 +0200

    map loader & destriy

--- src/components/BaseMap.vue
+++ src/components/BaseMap.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
 import mapboxgl, { Map, MapboxOptions } from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
-import { onMounted, onUnmounted, ref } from 'vue';
+import { onBeforeMount, onMounted, onUnmounted, ref } from 'vue';
+import { IonSpinner, IonText } from '@ionic/vue';
 
 const props = defineProps<{
     options?: MapboxOptions;
 }>();
 
 const mapRef = ref<HTMLElement | null>(null);
+const isMapLoading = ref<boolean>(false);
 const map = ref<Map | null>(null);
+
+onBeforeMount(() => {
+    isMapLoading.value = true
+});
 
 onMounted(() => {
     mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API_TOKEN;
@@ -28,19 +34,68 @@ onMounted(() => {
         ...props.options
     });
 
-    map.value?.once('render', () => {
-        map.value?.resize()
+    map.value?.once('load', () => {
+        map.value?.resize();
+        isMapLoading.value = false;
     });
 });
+
+onUnmounted(() => {
+    // destroy mapboxgl.Map instance
+    map.value?.remove();
+    map.value = null;
+})
 </script>
 
 <template>
-    <div ref="mapRef" class="map" />
+    <div class="map">
+        <Transition name="fade">
+            <div class="map__loader" v-show="isMapLoading" >
+                <ion-spinner name="circular" />
+
+                <p class="map__loader-message"><ion-text>loading map...</ion-text></p>
+            </div>
+        </Transition>
+
+        <div ref="mapRef" class="map__map" />
+    </div>
 </template>
 
 <style scoped>
 .map {
-  width: 100%;
-  height: 100%;
+    position: relative;
+}
+
+.map__map {
+    width: 100%;
+    height: 100%;
+}
+
+.map__loader {
+    position: absolute;
+    z-index: 1;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    background-color: white;
+}
+
+.map__loader-message {
+    margin-top: 8px;
+    font-size: 12px;
+} 
+
+/* TRANSITION */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 300ms ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
