18d012f163231d4f9993325eb44d4ec1ec027c4a
larsmarginet <lars.marginet@student.howest.be>
2023-08-07 18:59:07 +0200

    setup mapbox

---
+++ src/shims-vue.d.ts
@@ -0,0 +1,2 @@
+// make sure typescript can process .vue files
+declare module '*.vue';
---
+++ src/views/Tab1Page.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API_TOKEN;
+ 
+  const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+  });
+})
+</script>
+
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Tab 1</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 1</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <div id="map" class="map" />
+    </ion-content>
+  </ion-page>
+</template>
+
+<style scoped>
+.map {
+  width: 100%;
+  height: 100%;
+}
+</style>
+
+
