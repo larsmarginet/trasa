f14cf66e0b4b6341ca78ade304ba4f12a759a13c
larsmarginet <lars.marginet@student.howest.be>
2023-08-07 21:12:38 +0200

    render clustered gardens on map

--- src/components/BaseMap.vue
+++ src/components/BaseMap.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import mapboxgl, { Map, MapboxOptions } from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
-import { onBeforeMount, onMounted, onUnmounted, ref } from 'vue';
-import { IonSpinner, IonText } from '@ionic/vue';
+import { onBeforeMount, onMounted, onUnmounted, ref, watch } from "vue";
+import { IonSpinner, IonText } from "@ionic/vue";
 
 const props = defineProps<{
-    options?: MapboxOptions;
+  options?: MapboxOptions;
+  data?: any;
 }>();
 
 const mapRef = ref<HTMLElement | null>(null);
@@ -13,80 +14,162 @@ const isMapLoading = ref<boolean>(false);
 const map = ref<Map | null>(null);
 
 onBeforeMount(() => {
-    isMapLoading.value = true
+  isMapLoading.value = true;
 });
 
-onMounted(() => {
-    mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API_TOKEN;
+watch(
+  [() => props.data, () => map.value],
+  ([value]) => {
+    console.log("value update 1");
+    console.log(!map.value);
+    console.log(value);
+    if (map.value?.loaded && value) {
+      console.log("value update");
 
-    if (!mapRef.value) {
-        return;
+      map.value.addSource("gardens", {
+        type: "geojson",
+        data: value,
+        cluster: true,
+        clusterMaxZoom: 14, // Max zoom to cluster points on
+        clusterRadius: 50, // Radius of each cluster when clustering points (defaults to 50)
+      });
+
+      map.value.addLayer({
+        id: "clusters",
+        type: "circle",
+        source: "gardens",
+        filter: ["has", "point_count"],
+        paint: {
+          // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+          // with three steps to implement three types of circles:
+          //   * Blue, 20px circles when point count is less than 100
+          //   * Yellow, 30px circles when point count is between 100 and 750
+          //   * Pink, 40px circles when point count is greater than or equal to 750
+          "circle-color": [
+            "step",
+            ["get", "point_count"],
+            "#51bbd6",
+            100,
+            "#f1f075",
+            750,
+            "#f28cb1",
+          ],
+          "circle-radius": [
+            "step",
+            ["get", "point_count"],
+            20,
+            100,
+            30,
+            750,
+            40,
+          ],
+        },
+      });
+
+      map.value.addLayer({
+        id: "cluster-count",
+        type: "symbol",
+        source: "gardens",
+        filter: ["has", "point_count"],
+        layout: {
+          "text-field": ["get", "point_count_abbreviated"],
+          "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+          "text-size": 12,
+        },
+      });
+
+      map.value.addLayer({
+        id: "unclustered-point",
+        type: "circle",
+        source: "gardens",
+        filter: ["!", ["has", "point_count"]],
+        paint: {
+          "circle-color": "#11b4da",
+          "circle-radius": 4,
+          "circle-stroke-width": 1,
+          "circle-stroke-color": "#fff",
+        },
+      });
     }
+  },
+  { immediate: true }
+);
 
-    map.value = new mapboxgl.Map({
-        container: mapRef.value,
-        style: 'mapbox://styles/mapbox/streets-v12',
-        zoom: 9,
-        testMode: true,
-        minZoom: 6,
-        center: [3.729407, 51.065587],
-        // overwrite default options
-        ...props.options
-    });
+const renderMap = () => {
+  if (!mapRef.value) {
+    return;
+  }
 
-    map.value?.once('load', () => {
-        map.value?.resize();
-        isMapLoading.value = false;
-    });
+  map.value = new mapboxgl.Map({
+    container: mapRef.value,
+    style: "mapbox://styles/mapbox/streets-v12",
+    zoom: 3,
+    testMode: true,
+    // minZoom: 6,
+    center: [3.729407, 51.065587],
+    // overwrite default options
+    ...props.options,
+  });
+
+  map.value?.once("load", () => {
+    map.value?.resize();
+    isMapLoading.value = false;
+  });
+};
+
+onMounted(() => {
+  mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API_TOKEN;
+
+  renderMap();
 });
 
 onUnmounted(() => {
-    // destroy mapboxgl.Map instance
-    map.value?.remove();
-    map.value = null;
-})
+  // destroy mapboxgl.Map instance
+  map.value?.remove();
+  map.value = null;
+});
 </script>
 
 <template>
-    <div class="map">
-        <Transition name="fade">
-            <div class="map__loader" v-show="isMapLoading" >
-                <ion-spinner name="circular" />
+  <div class="map">
+    <Transition name="fade">
+      <div class="map__loader" v-show="isMapLoading">
+        <ion-spinner name="circular" />
 
-                <p class="map__loader-message"><ion-text>loading map...</ion-text></p>
-            </div>
-        </Transition>
+        <p class="map__loader-message"><ion-text>loading map...</ion-text></p>
+      </div>
+    </Transition>
 
-        <div ref="mapRef" class="map__map" />
-    </div>
+    <div ref="mapRef" class="map__map" />
+  </div>
 </template>
 
 <style scoped>
 .map {
-    position: relative;
+  position: relative;
 }
 
 .map__map {
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
 }
 
 .map__loader {
-    position: absolute;
-    z-index: 1;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    justify-content: center;
-    background-color: white;
+  position: absolute;
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  background-color: white;
 }
 
 .map__loader-message {
-    margin-top: 8px;
-    font-size: 12px;
-} 
+  margin-top: 8px;
+  font-size: 12px;
+}
 
 /* TRANSITION */
 .fade-enter-active,
--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,6 +1,63 @@
 <script setup lang="ts">
 import { BaseMap } from "@/components";
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue";
+import { onMounted, ref } from "vue";
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
+
+const FIREBASE_CONFIG = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DB_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+const app = initializeApp(FIREBASE_CONFIG);
+const db = getFirestore(app);
+
+const gardens = ref<any>({});
+
+const getAllListedGardens = async () => {
+  const q = query(collection(db, "campsites"), where("listed", "==", true));
+
+  const querySnapshot = await getDocs(q);
+
+  gardens.value = {
+    type: "FeatureCollection",
+    features: querySnapshot.docs.map((doc) => ({
+      type: "Feature",
+      properties: {
+        id: doc.id,
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [
+          doc.data().location.longitude,
+          doc.data().location.latitude,
+        ],
+      },
+    })),
+  };
+};
+
+onMounted(async () => {
+  await getAllListedGardens();
+});
 </script>
 
 <template>
@@ -17,8 +74,7 @@ import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue
         </ion-toolbar>
       </ion-header>
 
-      <BaseMap class="map" />
-
+      <BaseMap class="map" :data="gardens" />
     </ion-content>
   </ion-page>
 </template>
@@ -29,5 +85,3 @@ import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue
   height: 100%;
 }
 </style>
-
-
